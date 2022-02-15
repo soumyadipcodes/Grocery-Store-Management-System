ecddc8189d5c38271249648aaf81df4329823dfb
Soumyadip Mitra <97083283+soumyadip2148@users.noreply.github.com>
2022-02-15 09:00:48 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1466,7 +1466,7 @@ def employee_window():
     def welcome_bill():
         txtarea.delete("1.0",END)
         txtarea.insert(END,"\n \t\t\t\t   ABC GROCERY STORE ")
-        txtarea.insert(END,"\n \t\t\t\t   GT ROAD, RAJ GHAT")
+        txtarea.insert(END,"\n \t\t\t\t   MG ROAD, RAJ GHAT")
         txtarea.insert(END,"\n \t\t\t\t     DELHI-273005 ") 
         txtarea.insert(END,"\n \t\t\t\t PHONE: +91-1234567890 \n")
         txtarea.insert(END,"   "+str("=")*78)
